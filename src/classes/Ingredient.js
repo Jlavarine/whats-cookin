134bc4db9865603dba72e1b1f74e3f8359f9aac7
Jacob Lavarine <Jlavarine6237@gmail.com>
2022-03-27 20:12:29 -0500

    Add ingredient.js and ingredient-test.js files

---
+++ src/classes/Ingredient.js
@@ -0,0 +1,8 @@
+class Ingredient {
+  constructor(id, name, estimatedCost) {
+    this.id = id;
+    this.name = name;
+    this.estimatedCost = estimatedCost;
+  };
+};
+export default Ingredient;
---
+++ test/Ingredient-test.js
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+import Ingredient from '../src/classes/Ingredient';
+describe('Ingredient', () => {
+
+  it('should be an instance of ingredients', () => {
+    const newIngredient = new Ingredient(19116, 'marshmallow', 425);
+
+    expect(newIngredient).to.be.an.instanceof(Ingredient);
+  });
+  it('instantiate a new ingredient', () => {
+    const newIngredient = new Ingredient(19116, 'marshmallow', 425);
+
+    expect(newIngredient.id).to.equal(19116);
+    expect(newIngredient.name).to.equal('marshmallow');
+    expect(newIngredient.estimatedCost).to.equal(425);
+  });
+});
