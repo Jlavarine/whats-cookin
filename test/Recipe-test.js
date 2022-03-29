5998a838c6261821192ca149957efaecc5ffa265
Shane Warning <shane.k.warning@gmail.com>
2022-03-28 19:52:25 -0500

    Remove console.log

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -46,10 +46,10 @@ describe('Recipe', () => {
     });
     it('should have wheat flour as the first element', () => {
       expect(recipe.determineIngredientsNeeded()[0]).to.equal('wheat flour')
-      console.log(recipe.calculateCostofIngredients())
     });
     it('should be able to return the cost of a recipe', () => {
       expect(recipe.calculateCostofIngredients()).to.equal(177.76)
+      console.log(recipe.findInstructions())
     })
 
 });
