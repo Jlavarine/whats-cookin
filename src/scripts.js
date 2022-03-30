4af6399ff31f18243b9a62dcf21c3632c2ce83b3
Artan Myrtolli <artanmyrtolli@gmail.com>
2022-03-29 22:55:22 -0400

    Update scripts.js to populate all recipe cards on load

--- src/scripts.js
+++ src/scripts.js
@@ -2,11 +2,15 @@ import './styles.css';
 import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import RecipeRepository from './classes/RecipeRepository';
 
-const navButtons = document.querySelector('.nav')
-const mainRecipeDisplay = document.querySelector('.main__recipe-display-box')
+const navButtons = document.querySelector('.nav');
+const mainRecipeDisplay = document.querySelector('.main__recipe-images-box');
+const recipeHeader = document.querySelector('.main__recipe-header')
 const recipeCards = document.querySelectorAll('main__recipe-card')
 
+
+window.addEventListener('load', instantiateRecipeRepo)
 mainRecipeDisplay.addEventListener('click', (e) => {
     returnRecipe(e)
 })
@@ -23,6 +27,29 @@ function fireButton(event){
     if (event)
     console.log(event.target.dataset.button);
 }
+
+function instantiateRecipeRepo (){
+    const recipeRepo = new RecipeRepository();
+    recipeRepo.instantiateRecipes()
+    populateRecipeCards(recipeRepo)
+}
+
+
+function populateRecipeCards(recipeRepo) {
+    
+    recipeRepo.allRecipes.forEach((recipe, index) => {
+        mainRecipeDisplay.innerHTML +=  
+        `<div class="main__recipe-card" data-recipe="${recipeRepo.allRecipes[index].name}">
+        <img class="main__recipe-card-image" data-recipe="${recipeRepo.allRecipes[index].name}" src=${recipeRepo.allRecipes[index].image} alt="${recipeRepo.allRecipes[index].name}">
+        <p class="main__recipe-card-text" data-recipe="${recipeRepo.allRecipes[index].name}" >${recipeRepo.allRecipes[index].name}</p>
+         <div class="main__recipe-card-filters" data-recipe="${recipeRepo.allRecipes[index].name}">
+           <p class="main__recipe-card-tag" data-recipe="${recipeRepo.allRecipes[index].name}">${recipeRepo.allRecipes[index].tags}</p>
+          <p class="main__recipe-card-price" data-recipe="${recipeRepo.allRecipes[index].name}">$$$$</p>
+         </div>
+        </div> `
+    })
+}
+
 
 
 //add a function to create a recipe card w/ 
