89420fd471fbc80712898e0c3cdcceeed752288d
Jacob Lavarine <Jlavarine6237@gmail.com>
2022-04-02 17:47:43 -0500

    Refactor Recipe-repository-tests

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,32 +1,51 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 import Recipe from '../src/classes/Recipe';
-const data = require('../src/data/recipes');
+
+
+
+
+
 
 describe('RecipeRepository', () => {
   let recipeRepository;
   let recipe;
   beforeEach( () => {
-    recipe = new Recipe(data.recipeData[0].id, data.recipeData[0].image, data.recipeData[0].ingredients, data.recipeData[0].instructions, data.recipeData[0].name, data.recipeData[0].tags)
-    recipeRepository = new RecipeRepository(recipe);
+    recipe = new Recipe(1, 'example.com',
+    [{
+      id: 1,
+      name: 'pear',
+      estimatedCostInCents: 456,
+      quantity: {
+        amount: 1,
+        unit: 'c'
+      }
+    }], [{
+      instruction: 'cook.',
+      number: 1
+    }], 'Cookies', ['food','starter']);
+    recipeRepository = new RecipeRepository([recipe])
   });
 
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
+
   it('Should be an instance of recipe', () => {
     expect(recipeRepository).to.be.an.instanceof(RecipeRepository);
   });
-  it.skip('Should hold a list of all recipes', () => {
+
+  it('Should hold a list of all recipes', () => {
     recipeRepository.instantiateRecipes();
-    expect(recipeRepository.allRecipes.length).to.equal(50);
+    expect(recipeRepository.allRecipes.length).to.equal(1);
   });
-  it.skip('Should filter all recipes based on a tag', () => {
-    expect(recipeRepository.filterRecipesByTag('starter').length).to.equal(9);
-    expect(recipeRepository.filterRecipesByTag('starter')[0].name).to.equal("Loaded Chocolate Chip Pudding Cookie Cups");
+
+  it('Should filter all recipes based on a tag', () => {
+    expect(recipeRepository.filterRecipesByTag('starter').length).to.equal(1);
+    expect(recipeRepository.filterRecipesByTag('starter')[0].name).to.equal('Cookies');
   });
-  it.skip('Should filter all recipes based on a recipe name', () => {
-    expect(recipeRepository.filterRecipesByName("Loaded Chocolate Chip Pudding Cookie Cups").length).to.equal(1);
-    console.log(recipeRepository.filterRecipesByName("Cookie"))
+
+  it('Should filter all recipes based on a recipe name', () => {
+    expect(recipeRepository.filterRecipesByName("Cookies").length).to.equal(1);
   });
 });
