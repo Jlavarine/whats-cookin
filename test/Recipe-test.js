3dc4e619c683c656646522c245755e787e5a3560
Jacob Lavarine <Jlavarine6237@gmail.com>
2022-04-02 17:47:53 -0500

    Refactor Recipe-tests

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -1,55 +1,63 @@
 import { expect } from 'chai';
 import Recipe from '../src/classes/Recipe';
 import Ingredient from '../src/classes/Ingredient';
-const data = require('../src/data/recipes')
+
+
 
 
 describe('Recipe', () => {
     let recipe;
     beforeEach( () => {
-        recipe = new Recipe(data.recipeData[0].id, data.recipeData[0].image, data.recipeData[0].ingredients, data.recipeData[0].instructions, data.recipeData[0].name, data.recipeData[0].tags)
-    })
+        recipe = new Recipe(1, 'example.com',
+        [{
+          id: 1,
+          name: 'pear',
+          estimatedCostInCents: 456,
+          quantity: {
+            amount: 1,
+            unit: 'c'
+          }
+        }], [{
+          instruction: 'cook.',
+          number: 1
+        }], 'Cookies', ['food','starter']);
+    });
+
     it('should be an instance of Recipe', () => {
 
       expect(recipe).to.be.an.instanceof(Recipe);
     });
 
     it('should have an id', () => {
-        expect(recipe.id).to.equal(595736);
+        expect(recipe.id).to.equal(1);
     });
 
     it('should have an image', () => {
-        expect(recipe.image).to.equal("https://spoonacular.com/recipeImages/595736-556x370.jpg");
+        expect(recipe.image).to.equal('example.com');
     });
 
     it('should have ingredients', () => {
-        expect(recipe.ingredients).to.equal(data.recipeData[0].ingredients);
+        expect(recipe.ingredients.length).to.equal(1);
+        expect(recipe.ingredients[0].id).to.equal(1);
+
     });
 
     it('should have instructions', () => {
-        expect(recipe.instructions).to.equal(data.recipeData[0].instructions);
+        expect(recipe.instructions[0].instruction).to.equal('cook.');
     });
 
     it('should have a name', () => {
-        expect(recipe.name).to.equal("Loaded Chocolate Chip Pudding Cookie Cups");
+        expect(recipe.name).to.equal("Cookies");
     });
 
     it('should have tags', () => {
-        expect(recipe.tags).to.equal(data.recipeData[0].tags);
+        expect(recipe.tags).to.deep.equal(['food','starter']);
     });
 
-    it('should have all of the ingredients', () => {
-      recipe.instantiateIngredients();
-        expect(recipe.allIngredients.length).to.equal(247);
-    });
     it('should determine ingredients needed', () => {
-      expect(recipe.ingredients.length).to.equal(recipe.determineIngredientsNeeded().length)
+      expect(recipe.determineIngredientsNeeded(recipe.ingredients)).to.deep.equal(['pear'])
     });
-    it('should have wheat flour as the first element', () => {
-      expect(recipe.determineIngredientsNeeded()[0]).to.equal('wheat flour')
+    it('should determine cost of ingredients', () => {
+      expect(recipe.calculateCostofIngredients(recipe.ingredients)).to.equal(4.56)
     });
-    it('should be able to return the cost of a recipe', () => {
-      expect(recipe.calculateCostofIngredients()).to.equal(177.76)
-    })
-
 });
